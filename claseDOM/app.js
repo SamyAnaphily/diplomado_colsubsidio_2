d3e7a9199d6b0e16da2d2efaf776f9bade0ecc00
Sandra Castro <castrosandra0413@gmail.com>
2022-11-03 16:53:30 -0500

    siii lo hice

--- claseDOM/app.js
+++ claseDOM/app.js
@@ -4,11 +4,40 @@ const arrayElement = ['primer elemento','segundo','tercero']
 
 arrayElement.forEach(item => {
     console.log(item)
-    const li = document.createElement('li')
-    li.textContent = item
-    lista.appendChild(li)
+    const  elelista = document.createElement('li')
+    elelista.textContent = item
+    lista.appendChild(elelista);
 })
-/*arrayElement.forEach(item =>{
-
-})*/
-
+arrayElement.forEach(zapatos=>{
+         
+})
+const pets=[
+    {
+        id:1,
+        name:"Negro",
+        img:"./img/imagen1.jpg",
+    },
+    {
+        id:2,
+        name:"Blanco",
+        img:"./img/imagen2.jpg",
+    },
+    {
+        id:3,
+        name:"Tipo Tennis",
+        img:"./img/imagen3.jpg",
+    }
+];
+    
+let card = document.getElementById("card-template");
+ pets.map((x) =>{
+    card.innerHTML += `<div class="col">
+    <div class="card">
+      <img src="${x.img}" class="card-img-top" alt="${x.name}">
+      <div class="card-body">
+        <h5 class="card-title text-center text-primary">${x.name}</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+  </div> 
+  `;
+ })
