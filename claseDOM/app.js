b1b0ea4c38094c816843c5041804d78e3ce8f4b6
Sandra Castro <castrosandra0413@gmail.com>
2022-11-03 21:48:25 -0500

    cars

--- claseDOM/app.js
+++ claseDOM/app.js
@@ -1,43 +1,25 @@
-const lista = document.getElementById('lista')
-//console.log(lista)//
-const arrayElement = ['primer elemento','segundo','tercero']
+const card = document.getElementById("cards")
+card.setAttribute("class", "card")
 
-arrayElement.forEach(item => {
-    console.log(item)
-    const  elelista = document.createElement('li')
-    elelista.textContent = item
-    lista.appendChild(elelista);
-})
-arrayElement.forEach(zapatos=>{
-         
-})
-const pets=[
-    {
-        id:1,
-        name:"Negro",
-        img:"./img/imagen1.jpg",
-    },
-    {
-        id:2,
-        name:"Blanco",
-        img:"./img/imagen2.jpg",
-    },
-    {
-        id:3,
-        name:"Tipo Tennis",
-        img:"./img/imagen3.jpg",
-    }
-];
-    
-let card = document.getElementById("card-template");
- pets.map((x) =>{
-    card.innerHTML += `<div class="col">
-    <div class="card">
-      <img src="${x.img}" class="card-img-top" alt="${x.name}">
-      <div class="card-body">
-        <h5 class="card-title text-center text-primary">${x.name}</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-  </div> 
-  `;
- })
+
+ const div = document.createElement("div")
+ const img = document.createElement("img");
+ const h5 = document.createElement('h5');
+ const p = document.createElement('p')
+ const a = document.createElement('a')
+
+h5.textContent = "Zapatos para Dama"
+p.textContent = "Negros"
+a.textContent = "Comprar"
+
+div.setAttribute("class","style", "card-body", "width: 18rem;")
+h5.setAttribute("class","card-tittle")
+p.setAttribute("class","card-text")
+a.setAttribute("class","btn btn-primary")
+img.setAttribute("src","./img/imagen1.jpg")
+
+card.appendChild(div)
+div.appendChild(img)
+div.appendChild(h5)
+div.appendChild(p)
+div.appendChild(a)
